0cc61557f5821cff3dd1d6f8c0cc6e637ba0de0f
Tatiana Moraes <113339379+TatiM2801@users.noreply.github.com>
2024-07-27 14:34:34 +0100

    Generic Repository

--- SuperShopSLN/SuperShop/Startup.cs
+++ SuperShopSLN/SuperShop/Startup.cs
@@ -17,7 +17,6 @@ namespace SuperShop
 
         public IConfiguration Configuration { get; }
 
-        // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddDbContext<DataContext>(cfg =>
@@ -27,12 +26,11 @@ namespace SuperShop
 
             services.AddTransient<SeedDb>();
 
-            services.AddScoped<IRepository, Repository>();
+            services.AddScoped<IProductRepository, ProductRepository>();
 
             services.AddControllersWithViews();
         }
 
-        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
         {
             if (env.IsDevelopment())
@@ -42,9 +40,9 @@ namespace SuperShop
             else
             {
                 app.UseExceptionHandler("/Home/Error");
-                // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
                 app.UseHsts();
             }
+
             app.UseHttpsRedirection();
             app.UseStaticFiles();
 
