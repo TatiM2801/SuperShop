82a2a8cc941227a4298f9cf026699d6cfea44dab
Tatiana Moraes <113339379+TatiM2801@users.noreply.github.com>
2024-08-05 20:25:24 +0100

    BlobHelper
    BlobHelperSecret

--- SuperShopSLN/SuperShop/Data/Entities/Product.cs
+++ SuperShopSLN/SuperShop/Data/Entities/Product.cs
@@ -15,7 +15,7 @@ namespace SuperShop.Data.Entities
         public decimal Price { get; set; }
 
         [Display(Name = "Image")]
-        public string ImageUrl { get; set; }
+        public Guid ImageId { get; set; }
 
         [Display(Name = "Last Purchase")]
         public DateTime? LastPurchase { get; set; }
@@ -31,16 +31,8 @@ namespace SuperShop.Data.Entities
 
         public User User { get; set; }
 
-        public string ImageFullPath
-        {
-            get
-            {
-                if (string.IsNullOrEmpty(ImageUrl))
-                {
-                    return null;
-                }
-                return $"https://localhost:44386{ImageUrl.Substring(1)}";
-            }
-        }
+        public string ImageFullPath => ImageId == Guid.Empty
+            ? $"https://supershoptmp.azurewebsites.net/images/noimage.jpg"
+            : $"https://supershoptmp.blob.core.windows.net/products/{ImageId}";
     }
 }
--- SuperShopSLN/SuperShop/Helpers/IConverterHelper.cs
+++ SuperShopSLN/SuperShop/Helpers/IConverterHelper.cs
@@ -1,11 +1,12 @@
 ﻿using SuperShop.Data.Entities;
 using SuperShop.Models;
+using System;
 
 namespace SuperShop.Helpers
 {
     public interface IConverterHelper
     {
-        Product ToProduct(ProductViewModel model, string path, bool inNew);
+        Product ToProduct(ProductViewModel model, Guid imageId, bool inNew);
 
         ProductViewModel ToProductViewModel(Product product);
     }
